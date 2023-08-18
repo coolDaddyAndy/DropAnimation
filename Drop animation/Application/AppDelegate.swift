eb9b278a5fd1c046254842319577159402040778
Andrei Sushkou <AndreySushkov@My-Air.local>
2023-08-18 17:16:38 +0300

    Initial commit

---
+++ Drop animation/Application/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  Drop animation
+//
+//  Created by Andrei Sushkou on 18.08.23.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
+
---
+++ Drop animation/Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Drop animation
+//
+//  Created by Andrei Sushkou on 18.08.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .magenta
+    }
+
+
+}
+
